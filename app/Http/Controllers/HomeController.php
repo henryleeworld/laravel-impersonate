d6e005345630c6c5afaa77f6bc37c1a5d293cf4f
Henry Lee <henryleeworld@gmail.com>
2020-04-30 08:02:49 +0800

    feat:初始化

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\User;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        echo '目前登入者姓名：' . Auth::user()->name . PHP_EOL;
+        $otherUserId = 2;
+        Auth::user()->impersonate(User::findOrFail($otherUserId));
+        echo '切換使用者為：' . Auth::user()->name . PHP_EOL;
+        Auth::user()->leaveImpersonation();
+        echo '停止切換使用者。' . PHP_EOL;
+        echo '目前登入者姓名：' . Auth::user()->name . PHP_EOL;
+    }
+}
